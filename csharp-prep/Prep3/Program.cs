542afd0a2c678382302c02e279edbd64ad0bec72
aperfectskippy <aaronandreim@gmail.com>
2023-09-21 18:26:09 -0600

    Assignment 3 complete

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Console.Write("What is the magic number? ");
+        // int magicNumber = int.Parse(Console.ReadLine());
+
+        Random randomGenerator = new Random();
+        int magicNumber =randomGenerator.Next(1, 100);
+
+        int guessNumber = -1;
+
+        while (guessNumber != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            guessNumber = int.Parse(Console.ReadLine());
+
+            if (magicNumber > guessNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNumber < guessNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
+
     }
 }
