a38cb2f0ac2995f1f4870c944c25f02e2ac4295a
aaronandrei <aaronandreim@gmail.com>
2025-05-13 21:22:04 -0600

    exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101);
+
+        int userNumber = -1;
+
+        while (userNumber != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            userNumber = int.Parse(Console.ReadLine());
+
+            if (magicNumber > userNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNumber < userNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
+        
     }
 }
