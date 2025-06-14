268503763cbbd1cb26a6354d8bf3071b1c2c95f8
aaronandrei <aaronandreim@gmail.com>
2025-06-14 12:59:47 -0600

    homework assignment complete

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment a1 = new Assignment("Haley Marie", "Statistics");
+        Console.WriteLine(a1.GetSummary());
+
+        MathAssignment a2 = new MathAssignment("Kelton Michael", "Geometry", "1.1", "4-5");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+
+        WritingAssignment a3 = new WritingAssignment("Alina Adele", "Creative Writing", "Suspense Stories");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
     }
 }
